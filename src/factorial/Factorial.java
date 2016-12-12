2aca7e95b8f64496f21be09c96a3d626bcefd148
acomesanavila <acomesanavila@victoria7.danielcastelao.org>
2016-12-12 09:57:41 +0100

    eliminacion metodo multiplicacion

--- src/factorial/Factorial.java
+++ src/factorial/Factorial.java
@@ -1,13 +1,15 @@
 package factorial;
 
 public class Factorial {
-
+    
     public static void main(String[] args) {
+
         Metodos met = new Metodos();
-        int f = 1, j = 8;
+        int i,f = 1, j = 8;
 
-        met.multiplicacion(f, j);
-
+        for (i = j; i >= 1; i--) 
+            f = f * i;
+        
         met.amosar(f);
     }
 }
